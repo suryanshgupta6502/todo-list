c734ee2e29a126921d684d7d76eb8f3e68ecafbc
Suryansh <suryanshgupta6502@gmail.com>
2023-09-07 17:49:42 +0530

    deploy

--- app.js
+++ app.js
@@ -1,17 +1,18 @@
 const express = require("express")
 const bodyparser = require("body-parser")
 var app = express();
-const mongoose=require("mongoose")
+const mongoose = require("mongoose")
 
-var port=process.env.PORT || 3000
+// var port = 3001
 app.set("view engine", "ejs");
 app.use(bodyparser.urlencoded({ extended: true }))
 app.use(express.static("public"))
 var items = [];
 
-
-mongoose.connect("mongodb+srv://Surya:Surya@cluster0.yenhx0h.mongodb.net/?retryWrites=true&w=majority").then(()=>{console.log("succefully connected");})
-.catch((err)=>{console.log(err);})
+var url=process.env.MONGODB_URI ;
+// "mongodb+srv://zkhigfyexuegxhtiin:vercel@vercelmongodb.bdq8bhc.mongodb.net/?retryWrites=true&w=majority"
+mongoose.connect(url,{useNewUrlParser: true}).then(() => { console.log("succefully connected"); })
+    .catch((err) => { console.log(err); })
 
 const todoschema = mongoose.Schema({
     item: String
@@ -25,31 +26,31 @@ const defaultitems = [item1, item2]
 
 app.get("/", function (req, res) {
 
-    const items = Item.find({}).then((values)=>{
+    const items = Item.find({}).then((values) => {
         console.log(values);
-        if(values.length===0){
+        if (values.length === 0) {
             Item.insertMany(defaultitems).then(() => { console.log("succefule added to db"); })
-                            .catch(() => { console.log("Not added"); })
+                .catch(() => { console.log("Not added"); })
 
-        res.redirect("/");
+            res.redirect("/");
         }
-        else{
+        else {
             res.render("html", { todayday: "Today", newitem: values })
         }
-        
-    })
-    
-    .catch(()=>{console.log("not print");})
 
-    
+    })
+
+        .catch(() => { console.log("not print"); })
+
+
 
 })
 app.post("/", function (req, res) {
     const itemname = req.body.addnewitem
-    console.log("added to db -> "+itemname);
+    console.log("added to db -> " + itemname);
 
-    var item=new Item({
-        item:itemname
+    var item = new Item({
+        item: itemname
     })
     item.save();
     res.redirect("/")
@@ -59,18 +60,18 @@ app.post("/", function (req, res) {
     // res.send("this is post")
 })
 
-app.post("/delete",function(req,res){
+app.post("/delete", function (req, res) {
     console.log(req.body.checkbox)
-    Item.findByIdAndDelete(req.body.checkbox).then(()=>{console.log("deleted done");})
-                                    .catch(()=>{console.log("not deleted");})
+    Item.findByIdAndDelete(req.body.checkbox).then(() => { console.log("deleted done"); })
+        .catch(() => { console.log("not deleted"); })
 
-    res.redirect("/")                                
+    res.redirect("/")
 })
 
-app.get("/:routes",function(req,res){
+app.get("/:routes", function (req, res) {
     console.log(req.params.routes);
 })
 
-app.listen(port, function () {
-    console.log("localhost:3000")
-})
+
+var port=process.env.PORT || 3000;
+app.listen(port)
